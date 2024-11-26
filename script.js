ecdc181dabfc2036a6fa339c008f6fac8e15e510
saynotrish <anuran.nyleco24@gmail.com>
2024-11-26 13:35:10 +0800

    banner

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const burgir = document. querySelector(".burgir__menu")
-const headernav = document. querySelector(".header__nav")
+const headerNav = document. querySelector(".header__nav");
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("open"); //turns the burgir to x
     headerNav.classList.toggle("open"); //header nav appears
